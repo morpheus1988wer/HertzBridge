97d5c665b6bb06909eb39c3199fdad2a64b7b93f
Jonas Le-Späinghaus <jonasle-spainghaus@Jonass-MBP.localdomain>
2026-01-06 21:17:42 +0100

    Cleanup: Removed duplicated source folders and build artifacts

--- HertzBridge_v1 1.0_Source/Tests/HertzBridgeTests/HertzBridgeTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import HertzBridgeCore
-
-final class HertzBridgeTests: XCTestCase {
-    func testDeviceManagerShared() {
-        XCTAssertNotNil(DeviceManager.shared)
-    }
-}
